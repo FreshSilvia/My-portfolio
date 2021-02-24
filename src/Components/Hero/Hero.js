7bc91016841c10a895f04d26766f42c6d1bdd7e6
Jordan Howard <superjordan220@gmail.com>
2021-02-24 15:31:59 -0500

    Second update

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Profilep from '../../images/ProfessionalPic.jpg';
 
 function Hero() {
     return (
         <div className='hero-container'>
-            <div className='hero-text'>
-                <h1 className="title">Who is Jordan Howard</h1>
-                <div className="profile-pic">
-                    <img src={Profilep} alt="professionalpic" className="image"></img>
-                </div>
-            </div>
+            <h1>Jordan Howard</h1>
 
         </div>
     )
--- src/pages/Aboutme.js
+++ src/pages/Aboutme.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import Hero from '../Components/Hero/Hero';
-
+import Profilep from '../images/ProfessionalPic.jpg';
 
 const Aboutme = () => {
 
@@ -12,10 +12,15 @@ const Aboutme = () => {
     return (
         <>
             <Hero/>
-            <h1 className="me">About Me</h1>
-            <p className="info-text">
-                We are group of friends who love the need for speed, and want share that same love of motorsports with the rest of the car community. Here at GarageInsomnia, motorsports is more than just a sport, it's an art and lifestyle. Our job here is to provide our customers with great quality stock and performance automotive racing parts. Here you will find some of the lowest prices on Nissan aftermarket parts and other performance auto parts that you can relay on to relieve your need for speed.We only carry dependable performance parts that allow you to take your car to the next level in your build.We can offer some advice, great customer service, and the convenience of being able to shop right from your home, the track, and anywhere else you feel to upgrade your car. 
-            </p>
+            <div className="me-container">
+                <div className="profile-pic">
+                    <img src={Profilep} alt="professionalpic" className="image"></img>
+                    <p className="info-text">
+                        <h2>Hi, I'm Jordan</h2>
+                         We are group of friends who love the need for speed, and want share that same love of motorsports with the rest of the car community. Here at GarageInsomnia, motorsports is more than just a sport, it's an art and lifestyle. Our job here is to provide our customers with great quality stock and performance automotive racing parts. Here you will find some of the lowest prices on Nissan aftermarket parts and other performance auto parts that you can relay on to relieve your need for speed.We only carry dependable performance parts that allow you to take your car to the next level in your build.We can offer some advice, great customer service, and the convenience of being able to shop right from your home, the track, and anywhere else you feel to upgrade your car. 
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
